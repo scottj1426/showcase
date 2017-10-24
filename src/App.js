16662e2d66b2fc6adf4b78a2a76683ec248b5f02
James Scott <scottj1426@gmail.com>
2017-10-24 11:54:43 -0500

    first day react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import './index.css';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser'
+
+
+//topics
 
 class App extends Component {
   render() {
-    
+    return (
+      <TopicBrowser />
+    )
   }
 }
 
